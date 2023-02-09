e9898ed46a2ed530f5592db138658450ed7a6c55
beengineer <beengineer.student@gmail.com>
2023-02-10 02:57:33 +0900

    0209renew

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", function () {
   const alertBtn = document.getElementById("alert-btn");
   alertBtn.addEventListener("click", function (e) {
-    window.alert("これはアラートです。");
-  });
+  document.getElementById("title").style.color = "rgb(0,0,0)"
+ document.getElementById("title").style.fontFamily = "Caveat"
+});
 });
